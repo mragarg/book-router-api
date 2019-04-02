4facd06c75b24e5cb24dc3bbab5c8ba074d1f562
Ashish Garg <mragarg@gmail.com>
2019-04-02 13:27:57 -0400

    Creates book router

---
+++ routes/book.js
@@ -0,0 +1,17 @@
+// Express
+const express = require('express');
+
+// Express Router
+const Router = express.Router;
+
+// Create the Router 
+const bookRoutes = Router();
+
+// Handlers for my routers
+// GET
+
+// POST
+
+// PUT
+
+// DELETE
