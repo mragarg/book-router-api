76a783110f0f9a3c7a8dcff5062c50f125d03cd1
Ashish Garg <mragarg@gmail.com>
2019-04-02 13:35:01 -0400

    Adds get router

--- routes/book.js
+++ routes/book.js
@@ -4,11 +4,15 @@ const express = require('express');
 // Express Router
 const Router = express.Router;
 
+// Controller
+const controller = require('../controllers/book');
+
 // Create the Router 
 const bookRoutes = Router();
 
 // Handlers for my routers
 // GET
+bookRoutes.get('/', controller.create);
 
 // POST
 
